b4235ea3f7bd3a4dcb68241bc5cd94465a2f44a7
Moh <mohakemal9@gmail.com>
2017-08-19 06:35:52 -0400

    file added

---
+++ fahrenheit-to-centigrade.c
@@ -0,0 +1,20 @@
+﻿/* This is a simple application to do some practice on how to change fahreneit to degree centigrade */
+
+#include <stdio.h>
+
+int main(void) {
+     
+	float fahrenheit,centigrade;
+	
+	printf("%f", &fahrenheit);
+
+	centigrade = (fahrenheit -32.0) * 5.0/9.0;
+	
+	printf("\n\n Fahrenheit to Celsius Converter\n\n\tCentigrade temp is %f",
+
+		centigrade);
+	
+	return 0;
+
+
+}
